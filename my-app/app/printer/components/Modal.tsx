7f34532e086a5dc641a37e2820150880c0ea87dd
aomchyn <orm.mee19@gmail.com>
2026-01-17 11:57:35 +0700

    Initial commit

---
+++ my-app/app/printer/components/Modal.tsx
@@ -0,0 +1,67 @@
+'use client'
+
+import { KeyboardEvent, useEffect , useRef } from "react"
+
+interface ModalProps{
+    id?: string
+    size?:'sm' | 'md' | 'lg' | 'xl'
+    title?:string
+    children: React.ReactNode
+    onClose:() => void
+}
+
+const Modal = ({id,size = 'md', title,children,onClose} : ModalProps) => {
+    const modalRef = useRef<HTMLDivElement>(null)
+
+    useEffect(() => {
+        const handleEscape = (e:KeyboardEvent) => {
+            if (e.key === 'Escape'){
+                onClose()
+            }
+        }
+
+        const handleClickOutSide = (e:MouseEvent) => {
+            if (modalRef.current && !modalRef.current.contains(e.target as Node)){
+                onClose()
+            }
+        }
+
+        document.addEventListener('keydown',handleEscape)
+        document.addEventListener('mousedown',handleClickOutSide)
+
+        return () => {
+            document.removeEventListener('keydown',handleEscape)
+            document.removeEventListener('mousedown',handleClickOutSide)
+        }
+    },[onClose])
+
+    const sizeClasses = {
+        sm:'max-w-sm',
+        md:'max-w-md',
+        lg:'max-w-lg',
+        xl:'max-w-xl'
+    }
+
+    return (
+              <div className="modal">
+                <div
+                ref = {modalRef}
+                id= {id}
+                className={`${sizeClasses[size]} modal-container`}>
+                    <div className="modal-header">
+                        <h3 className="modal-title">
+                            {title}
+                        </h3>
+                        <button
+                        onClick={onClose}
+                        className="modal-close">
+                            <i className="fas fa-times"></i>
+                        </button>
+                    </div>
+                    <div className="modal-body">
+                        {children}
+                    </div>
+                </div>
+              </div>
+    );
+}
